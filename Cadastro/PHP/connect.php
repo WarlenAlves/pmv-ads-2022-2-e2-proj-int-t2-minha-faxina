dd85a1b9d15f904ab807c63e500f2143efe69218
Rafael Souza <rafael.souza54@hotmail.com>
2022-10-06 20:47:30 -0300

    PHP

---
+++ Cadastro/PHP/connect.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '@Rosimary8971';
+$dbname = 'cadastro';
+
+//criar conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
