4862122855bbfbc5db45e3f5e24202ee6675cfdb
TioTaylor <1083465@sga.pucminas.br>
2022-10-25 21:57:38 -0300

    Desenvolvimento-OLD

--- Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace PucWebApplication.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
